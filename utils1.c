e57ac30110ea8e6c286c5776cb654d0459a31cdf
Solitarus <48644498+soliter7@users.noreply.github.com>
2019-07-21 17:12:44 +0200

    lol

---
+++ utils1.c
@@ -0,0 +1,36 @@
+#include "rush02.h"
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return ((unsigned)s1[i] - (unsigned)s2[i]);
+}
+
+
+int ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (to_find[j] == str[i + j])
+		{
+			if (to_find[j + 1] == '\0')
+			{
+				return (i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
